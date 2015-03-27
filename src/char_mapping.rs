f9e7253e75ff7cbca45637c423c8b6657f9bf7e6
Matthew Shapiro <me@mshapiro.net>
2015-03-27 13:42:45 -0400

    Removed code looking for hashmap compatibility, as it was replaced via concatenation

--- src/char_mapping.rs
+++ src/char_mapping.rs
@@ -1,5 +1,4 @@
 use std::collections::hash_map::{HashMap, Entry};
-use types;
 
 pub fn update_mappings(input: &str, candidate: &str, mapping: &mut HashMap<char, char>) {
 	if input.len() != candidate.len() {
@@ -14,24 +13,6 @@ pub fn update_mappings(input: &str, candidate: &str, mapping: &mut HashMap<char,
 			Entry::Occupied(_) => () /* ignore */
 		}; 
 	}
-}
-
-pub fn get_compatibility(left: &HashMap<char, char>, right: &HashMap<char, char>) -> types::Compatibility {
-	let mut total_matches = 0;
-	for (left_key, left_value) in left.iter() {
-		match right.get(left_key) {
-			Some(right_value) => {
-				if left_value != right_value { 
-					return types::Compatibility::Incompatible 
-				} else {
-					total_matches = total_matches + 1;
-				}
-			},
-			None => ()
-		}
-	}
-
-	return types::Compatibility::TotalMatches(total_matches);
 }
 
 pub fn get_map_concatenation(maps: Vec<&HashMap<char, char>>) -> Option<HashMap<char, char>> {
@@ -56,7 +37,6 @@ pub fn get_map_concatenation(maps: Vec<&HashMap<char, char>>) -> Option<HashMap<
 mod tests {
 	use super::*;
 	use std::collections::HashMap;
-	use types;
 
 	#[test]
 	fn can_correctly_set_character_mapping() {
@@ -71,38 +51,6 @@ mod tests {
 		assert_eq!(mapping.get(&'b'), Some(&'x'));
 		assert_eq!(mapping.get(&'c'), Some(&'y'));
 		assert_eq!(mapping.get(&'d'), Some(&'z'));
-	}
-
-	#[test]
-	fn hash_maps_with_same_key_value_pairs_shows_total_matches() {
-		let mut left = HashMap::new();
-		let mut right = HashMap::new();
-
-		left.insert('a', 'b');
-		left.insert('b', 'c');
-		left.insert('c', 'd');
-
-		right.insert('d', 'e');
-		right.insert('a', 'b');
-		right.insert('c', 'd');
-
-		let result = get_compatibility(&left, &right);
-		assert_eq!(result, types::Compatibility::TotalMatches(2));
-	}
-
-	#[test]
-	fn hash_maps_with_conflicting_values_are_incompatible() {
-		let mut left = HashMap::new();
-		let mut right = HashMap::new();
-
-		left.insert('a', 'b');
-		left.insert('b', 'c');
-
-		right.insert('b', 'd');
-		right.insert('a', 'b');
-
-		let result = get_compatibility(&left, &right);
-		assert_eq!(result, types::Compatibility::Incompatible);
 	}
 
 	#[test]
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::fmt;
 
 #[derive(Clone)]
 pub struct FingerprintedValue {
@@ -16,21 +15,4 @@ pub struct InputValue {
 pub struct Possibility {
 	pub fingerprint: FingerprintedValue,
 	pub char_mapping: HashMap<char, char>
-}
-
-#[derive(PartialEq)]
-pub enum Compatibility {
-	TotalMatches(i32),
-	Incompatible
-}
-
-impl fmt::Debug for Compatibility {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let result = match *self {
-			Compatibility::Incompatible => write!(f, "Incompatible"),
-			Compatibility::TotalMatches(x) => write!(f, "Total Matches: {}", x)
-		};
-
-		return result;
-	}
 }
